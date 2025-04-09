6463302b0d3f5ffae9e396defed5922d85790b92
Victor <joaouns@gmail.com>
2025-04-09 11:52:15 -0300

    Adicionando menu e ajustes

--- js/modo-claro-escuro.js
+++ js/modo-claro-escuro.js
@@ -2,19 +2,35 @@ document.addEventListener("DOMContentLoaded", function () {
     const botaoAlternar = document.getElementById("alternar-tema");
     const body = document.body;
 
-    
+    const botaoClaro = document.getElementById("tema-claro");
+    const botaoEscuro = document.getElementById("tema-escuro");
+
     if (localStorage.getItem("tema") === "claro") {
         body.setAttribute("data-theme", "claro");
-        botaoAlternar.checked = true;
+        if (botaoAlternar) botaoAlternar.checked = true;
     }
 
-    botaoAlternar.addEventListener("change", function () {
-        if (botaoAlternar.checked) {
-            body.setAttribute("data-theme", "claro");
-            localStorage.setItem("tema", "claro");
-        } else {
-            body.setAttribute("data-theme", "escuro");
-            localStorage.setItem("tema", "escuro");
-        }
+    if (botaoAlternar) {
+        botaoAlternar.addEventListener("change", function () {
+            if (botaoAlternar.checked) {
+                body.setAttribute("data-theme", "claro");
+                localStorage.setItem("tema", "claro");
+            } else {
+                body.setAttribute("data-theme", "escuro");
+                localStorage.setItem("tema", "escuro");
+            }
+        });
+    }
+
+    botaoClaro.addEventListener("click", function () {
+        body.setAttribute("data-theme", "claro");
+        localStorage.setItem("tema", "claro");
+        if (botaoAlternar) botaoAlternar.checked = true;
+    });
+
+    botaoEscuro.addEventListener("click", function () {
+        body.setAttribute("data-theme", "escuro");
+        localStorage.setItem("tema", "escuro");
+        if (botaoAlternar) botaoAlternar.checked = false;
     });
 });
