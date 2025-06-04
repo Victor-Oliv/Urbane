65f8956f90394f8c59ed253f568bbb71f308dea8
Victor <joaouns@gmail.com>
2025-06-04 20:35:09 -0300

    olho do login

--- js/login.js
+++ js/login.js
@@ -95,5 +95,22 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+const inputSenha = document.getElementById("Senha");
+if (inputSenha) {
+  inputSenha.addEventListener("click", function (e) {
+    const cliqueProximoDoOlho = e.offsetX > inputSenha.clientWidth - 40;
+    if (!cliqueProximoDoOlho) return;
+
+    if (inputSenha.type === "password") {
+      inputSenha.type = "text";
+      inputSenha.classList.add("olho-aberto");
+    } else {
+      inputSenha.type = "password";
+      inputSenha.classList.remove("olho-aberto");
+    }
+  });
+}
+
+
 
 
